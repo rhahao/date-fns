98c6c1033582e31ceb1ea709f091e0cee404c0a4
Sasha Koss <koss@nocorp.me>
2022-06-29 12:06:38 +0800

    Upgrade TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,20 @@
 module.exports = {
-  extends: ['eslint:recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
+
+  plugins: ['@typescript-eslint'],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+
   env: {
     browser: true,
     node: true,
     es6: true,
   },
+
   rules: {
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'no-redeclare': 'off',
